6b4537faabe6e2c6bb5177d2ffb4e9dd9fe3d4bc
VivekGujale <vdgujale007@.com>
2023-01-09 16:10:06 +0530

    Create html file to test services with json server

---
+++ js files/site_properties.js
@@ -0,0 +1,7 @@
+//this js file is defined to help reducing hard coded values in app and use dry principle->do not repeat yourself..
+let site_properties = {
+    home_page: "../pages/payrollHome.html",
+    emp_payroll_page: "../pages/payrollForm.html",
+    use_local_storage: "false",
+    server_url: "  http://localhost:3000/EmployeePayrollDB/"
+}; 
